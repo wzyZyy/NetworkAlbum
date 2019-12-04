cdf52f0a9babee5f940cbbb02991bb56c1ab73bb
wangzhengyang <zy_wao@163.com>
2019-12-04 20:47:03 +0800

    ImageController ImageService uploadImage()

--- src/main/java/cn/zuel/wlyw/controller/ImageController.java
+++ src/main/java/cn/zuel/wlyw/controller/ImageController.java
@@ -1,6 +1,29 @@
 package cn.zuel.wlyw.controller;
 
+import cn.zuel.wlyw.kit.BaseResponse;
+import cn.zuel.wlyw.kit.ResultCodeEnum;
+import cn.zuel.wlyw.service.ImageService;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
+import com.jfinal.upload.UploadFile;
+
+import java.util.List;
 
 public class ImageController extends Controller {
+    ImageService imageService = new ImageService();
+
+    /**
+     * 上传图片
+     */
+    public void uploadImages() {
+        BaseResponse baseResponse = new BaseResponse();
+        String i_a_id = getPara("i_a_id");
+        List<UploadFile> images = getFiles("files");
+        if (!StrKit.isBlank(i_a_id)) {
+            baseResponse = imageService.uploadImages(i_a_id, images);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
+    }
 }
--- src/main/java/cn/zuel/wlyw/service/ImageService.java
+++ src/main/java/cn/zuel/wlyw/service/ImageService.java
@@ -1,4 +1,51 @@
 package cn.zuel.wlyw.service;
 
+import cn.zuel.wlyw.kit.BaseResponse;
+import cn.zuel.wlyw.kit.ResultCodeEnum;
+import cn.zuel.wlyw.model.Image;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.IAtom;
+import com.jfinal.upload.UploadFile;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class ImageService {
+    /**
+     * 上传图片
+     *
+     * @param i_a_id
+     * @param uploadFiles
+     * @return
+     */
+    public BaseResponse uploadImages(final String i_a_id, final List<UploadFile> uploadFiles) {
+        final BaseResponse baseResponse = new BaseResponse();
+
+        final boolean succeed = Db.tx(new IAtom() {
+            boolean result = true;
+
+            @Override
+            public boolean run() throws SQLException {
+                for (UploadFile image :
+                        uploadFiles) {
+                    String i_name = image.getFileName();
+                    String i_path = "/upload/" + i_name;
+                    Image image1 = new Image();
+                    image1.setIAId(Integer.parseInt(i_a_id));
+                    image1.setIName(i_name);
+                    image1.setIPath(i_path);
+                    if (!image1.save()) {
+                        result = false;
+                    }
+                }
+                return result;
+            }
+        });
+        if (succeed) {
+            baseResponse.setResult(ResultCodeEnum.GOODS_UPLOAD_SUCCESS);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.GOODS_UPLOAD_FAILURE_DB_ERROR);
+        }
+        return baseResponse;
+    }
 }
