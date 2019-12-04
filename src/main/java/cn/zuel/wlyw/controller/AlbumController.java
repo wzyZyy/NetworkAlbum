8f42e8d341b07f19e43dde630e9a0e23f235e9bd
wangzhengyang <zy_wao@163.com>
2019-12-04 19:57:40 +0800

    AlbumController AlbumService ResultCodeEnum

--- src/main/java/cn/zuel/wlyw/controller/AlbumController.java
+++ src/main/java/cn/zuel/wlyw/controller/AlbumController.java
@@ -1,6 +1,87 @@
 package cn.zuel.wlyw.controller;
 
+import cn.zuel.wlyw.kit.BaseResponse;
+import cn.zuel.wlyw.kit.ResultCodeEnum;
+import cn.zuel.wlyw.service.AlbumService;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
 
 public class AlbumController extends Controller {
+    AlbumService albumService = new AlbumService();
+
+    /**
+     * 添加相册
+     */
+    public void createAlbum() {
+        BaseResponse baseResponse = new BaseResponse();
+        String a_u_id = getPara("a_u_id");
+        String a_t_id = getPara("a_t_id");
+        String a_name = getPara("a_name");
+        String a_desc = getPara("a_desc");
+        String a_auth = getPara("a_auth");
+
+        if (!StrKit.isBlank(a_u_id) && !StrKit.isBlank(a_t_id) && !StrKit.isBlank(a_name) && !StrKit.isBlank(a_auth)) {
+            // 需要的相册信息完整
+            baseResponse = albumService.createAlbum(a_u_id, a_t_id, a_name, a_desc, a_auth);
+        } else {
+            // 提交的必要表单信息不完整
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
+    }
+
+    /**
+     * 获取所有的相册
+     */
+    public void getAlbums() {
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse = albumService.getAlbums();
+        renderJson(baseResponse);
+    }
+
+    /**
+     * 获取某一相册
+     */
+    public void getOneAlbum() {
+        BaseResponse baseResponse = new BaseResponse();
+        String a_id = getPara("a_id");
+        if (!StrKit.isBlank(a_id)) {
+            baseResponse = albumService.getOneAlbum(a_id);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
+    }
+
+    /**
+     * 删除相册
+     */
+    public void deleteAlbum() {
+        BaseResponse baseResponse = new BaseResponse();
+        String a_id = getPara("a_id");
+        if (!StrKit.isBlank(a_id)) {
+            baseResponse = albumService.deleteAlbum(a_id);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
+    }
+
+    /**
+     * 修改相册的信息
+     */
+    public void modifyAlbum() {
+        BaseResponse baseResponse = new BaseResponse();
+        String a_id = getPara("a_id");
+        String a_t_id = getPara("a_t_id");
+        String a_name = getPara("a_name");
+        String a_desc = getPara("a_desc");
+        String a_auth = getPara("a_auth");
+        if (!StrKit.isBlank(a_id) && !StrKit.isBlank(a_t_id) && !StrKit.isBlank(a_name) && !StrKit.isBlank(a_desc) && !StrKit.isBlank(a_auth)) {
+            baseResponse = albumService.modifyAlbum(a_id, a_t_id, a_name, a_desc, a_auth);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
+    }
 }
--- src/main/java/cn/zuel/wlyw/service/AlbumService.java
+++ src/main/java/cn/zuel/wlyw/service/AlbumService.java
@@ -1,4 +1,140 @@
 package cn.zuel.wlyw.service;
 
+import cn.zuel.wlyw.kit.BaseResponse;
+import cn.zuel.wlyw.kit.ResultCodeEnum;
+import cn.zuel.wlyw.model.Album;
+
+import java.util.List;
+
 public class AlbumService {
+    /**
+     * 添加相册
+     *
+     * @param a_u_id
+     * @param a_t_id
+     * @param a_name
+     * @param a_desc
+     * @param a_auth
+     * @return
+     */
+    public BaseResponse createAlbum(String a_u_id, String a_t_id, String a_name, String a_desc, String a_auth) {
+        BaseResponse baseResponse = new BaseResponse();
+        List<Album> albums = Album.dao.find("select * from user where a_name = " + "'" + a_name + "'");
+        if (albums.size() == 0) {
+            // 相册不存在，可以添加
+            Album album = new Album();
+            album.setAUId(Integer.parseInt(a_u_id));
+            album.setATId(Integer.parseInt(a_t_id));
+            album.setAName(a_name);
+            album.setADesc(a_desc);
+            album.setAAuth(Integer.parseInt(a_auth));
+            if (album.save()) {
+                // 添加成功
+                baseResponse.setResult(ResultCodeEnum.REGISTER_SUCCESS);
+            } else {
+                // 添加失败，数据库操作错误
+                baseResponse.setResult(ResultCodeEnum.REGISTER_FAILURE_DB_ERROR);
+            }
+        } else if (albums.size() == 1) {
+            // 添加失败，相册已经存在
+            baseResponse.setResult(ResultCodeEnum.REGISTER_FAILURE_USER_EXIST);
+        } else {
+            // 添加失败，未知的系统错误
+            baseResponse.setResult(ResultCodeEnum.REGISTER_FAILURE_SYS_ERROR);
+        }
+        return baseResponse;
+    }
+
+    /**
+     * 获取所有的相册
+     *
+     * @return
+     */
+    public BaseResponse getAlbums() {
+        BaseResponse baseResponse = new BaseResponse();
+        List<Album> albums = Album.dao.find("select * from album");
+        if (albums.size() > 0) {
+            // 查找相册不为空
+            baseResponse.setData(albums);
+            baseResponse.setResult(ResultCodeEnum.USER_QUERY_SUCCESS);
+        } else {
+            // 相册为空
+            baseResponse.setResult(ResultCodeEnum.RECORD_NO_EXIST);
+        }
+        return baseResponse;
+    }
+
+    /**
+     * 查找某一相册
+     *
+     * @param a_id
+     * @return
+     */
+    public BaseResponse getOneAlbum(String a_id) {
+        BaseResponse baseResponse = new BaseResponse();
+        Album album = Album.dao.findFirst("select * from album where a_id = " + "'" + a_id + "'");
+        if (album != null) {
+            // 查找相册不为空
+            baseResponse.setData(album);
+            baseResponse.setResult(ResultCodeEnum.USER_QUERY_SUCCESS);
+        } else {
+            // 该相册不存在
+            baseResponse.setResult(ResultCodeEnum.RECORD_NO_EXIST);
+        }
+        return baseResponse;
+    }
+
+    /**
+     * 删除相册
+     *
+     * @param a_id
+     * @return
+     */
+    public BaseResponse deleteAlbum(String a_id) {
+        BaseResponse baseResponse = new BaseResponse();
+        Album album = Album.dao.findFirst("select * from album where a_id = " + "'" + a_id + "'");
+        if (album != null) {
+            // 查找相册不为空
+            if (album.delete()) {
+                baseResponse.setResult(ResultCodeEnum.DB_DELETE_SUCCESS);
+            } else {
+                baseResponse.setResult(ResultCodeEnum.DB_DELETE_FAILURE);
+            }
+        } else {
+            // 该相册不存在
+            baseResponse.setResult(ResultCodeEnum.RECORD_NO_EXIST);
+        }
+        return baseResponse;
+    }
+
+    /**
+     * 修改相册的信息
+     *
+     * @param a_id
+     * @param a_t_id
+     * @param a_name
+     * @param a_desc
+     * @param a_auth
+     * @return
+     */
+    public BaseResponse modifyAlbum(String a_id, String a_t_id, String a_name, String a_desc, String a_auth) {
+        BaseResponse baseResponse = new BaseResponse();
+        Album album = Album.dao.findFirst("select * from album where a_id = " + "'" + a_id + "'");
+        if (album != null) {
+            // 查找相册不为空
+            album.setATId(Integer.parseInt(a_t_id));
+            album.setAName(a_name);
+            album.setADesc(a_desc);
+            album.setAAuth(Integer.parseInt(a_auth));
+            if (album.save()) {
+                baseResponse.setResult(ResultCodeEnum.DB_UPDATE_SUCCESS);
+            } else {
+                baseResponse.setResult(ResultCodeEnum.DB_UPDATE_ERROR);
+            }
+        } else {
+            // 该相册不存在
+            baseResponse.setResult(ResultCodeEnum.RECORD_NO_EXIST);
+        }
+        return  baseResponse;
+    }
 }
